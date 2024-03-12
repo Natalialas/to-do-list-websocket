3342435ad7eca8f93e837f58d2a7ebbf45aeeb09
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-03-12 11:57:17 +0100

    Add sockets to server

--- server.js
+++ server.js
@@ -8,6 +8,27 @@ const server = app.listen(8000, () => {
 });
 const io = socket(server);
 
+const tasks = []
+
+io.on('connection', (socket) => {
+    socket.emit('updateData', tasks);
+
+    socket.on('addTask', (task) => {
+        tasks.push(task);
+        socket.broadcast.emit('addTask', task);
+    });
+
+    socket.on('removeTask', (taskId) => {
+        tasks = tasks.filter(task => task.id !== taskId);
+        socket.broadcast.emit('removeTask', taskId);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Socket ' + socket.id + ' disconnected');
+    });
+});
+
+
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found...' });
 });
